30dc7a4f2b29ad989cf7cf3a95e3b0ad0bfedde0
MarcoA-Pozol <marcoantoniopozolnarciso@gmail.com>
2025-03-20 21:32:39 -0600

    Restructure for ShopingCart component child components

--- src/components/ShopingCartSystemComponents/ItemComponent.js
+++ src/components/ShopingCartSystemComponents/ItemComponent.js
@@ -3,13 +3,36 @@ import "../../styles/ItemComponent.css";
 import RemoveAddButton from "./RemoveAddButtons.js";
 
 const ItemComponent = ({name, image_url, price}) => {
+	
+	const [itemOnCart, setItemOnCart] = useState(false);
+    const [itemAmount, setItemAmount] = useState(0);
+
+    function removeItem() {
+        if (itemOnCart === true && itemAmount === 1) {
+            setItemOnCart(false)
+            setItemAmount(0);
+        } else if (itemOnCart === true && itemAmount > 1) {
+            setItemAmount(itemAmount-1);
+        } else {
+            console.log("There was a counting error during removing item from shoping cart.");
+        }
+    }
+
+    function addItem() {
+        if (itemOnCart === false && itemAmount === 0) {
+            setItemOnCart(true);
+            setItemAmount(itemAmount + 1);
+        } else if (itemOnCart === true && itemAmount >= 1) {
+            setItemAmount(itemAmount+1);
+        } 
+    }
 
     return (
         <div className="item-card">
             <h2>{name}</h2>
             <img src={image_url} alt={name} width="200"/>
             <p>$ <strong>{price}</strong></p>
-            <RemoveAddButton/>
+            <RemoveAddButton removeItem={removeItem} addItem={addItem} itemAmount={itemAmount}/>
         </div>
     );
 }
